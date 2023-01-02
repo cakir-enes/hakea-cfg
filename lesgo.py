24f22998c6d6e2947d39187c9f8b360e9b611d32
cakir-enes <cakir-enes@outlook.com>
2023-01-02 23:54:45 +0300

    ew

---
+++ lesgo.py
@@ -0,0 +1,185 @@
+map = [
+    ["Esc", 0],
+    ["1", 1],
+    ["2", 2],
+    ["3", 3],
+    ["4", 4],
+    ["5", 5],
+    ["6", 6],
+    ["7", 7],
+    ["8", 8],
+    ["9", 9],
+    ["0", 10],
+    ["BSPC", 11],
+    ["Tab", 12],
+    ["Q", 13],
+    ["W", 14],
+    ["E", 15],
+    ["R", 16],
+    ["T", 17],
+    ["Y", 18],
+    ["U", 19],
+    ["I", 20],
+    ["O", 21],
+    ["P", 22],
+    ["\\", 23],
+    ["Meta", 24],
+    ["A", 25],
+    ["S", 26],
+    ["D", 27],
+    ["F", 28],
+    ["G", 29],
+    ["H", 30],
+    ["J", 31],
+    ["K", 32],
+    ["L", 33],
+    [";", 34],
+    ["'", 35],
+    ["LShift", 36],
+    ["Z", 37],
+    ["X", 38],
+    ["C", 39],
+    ["V", 40],
+    ["B", 41],
+    ["N", 44],
+    ["M", 45],
+    [",", 46],
+    [".", 47],
+    ["/", 48],
+    ["RShift", 49],
+    ["Ctrl", 50],
+    ["LSpace", 51],
+    ["Raise", 52],
+    ["Lower", 53],
+    ["RSpace", 54],
+    ["Alt", 55],
+]
+keys = [
+    {"label": "Esc", "row": 0, "col": 0, "x": 0, "y": 0.93},
+    {"label": "1", "row": 0, "col": 1, "x": 1, "y": 0.93},
+    {"label": "2", "row": 0, "col": 2, "x": 2, "y": 0.31},
+    {"label": "3", "row": 0, "col": 3, "x": 3, "y": 0},
+    {"label": "4", "row": 0, "col": 4, "x": 4, "y": 0.28},
+    {"label": "5", "row": 0, "col": 5, "x": 5, "y": 0.42},
+    {"label": "6", "row": 0, "col": 6, "x": 9, "y": 0.42},
+    {"label": "7", "row": 0, "col": 7, "x": 10, "y": 0.28},
+    {"label": "8", "row": 0, "col": 8, "x": 11, "y": 0},
+    {"label": "9", "row": 0, "col": 9, "x": 12, "y": 0.31},
+    {"label": "0", "row": 0, "col": 10, "x": 13, "y": 0.93},
+    {"label": "BSPC", "row": 0, "col": 11, "x": 14, "y": 0.93},
+    {"label": "Tab", "row": 1, "col": 0, "x": 0, "y": 1.93},
+    {"label": "Q", "row": 1, "col": 1, "x": 1, "y": 1.93},
+    {"label": "W", "row": 1, "col": 2, "x": 2, "y": 1.31},
+    {"label": "E", "row": 1, "col": 3, "x": 3, "y": 1},
+    {"label": "R", "row": 1, "col": 4, "x": 4, "y": 1.28},
+    {"label": "T", "row": 1, "col": 5, "x": 5, "y": 1.42},
+    {"label": "Y", "row": 1, "col": 6, "x": 9, "y": 1.42},
+    {"label": "U", "row": 1, "col": 7, "x": 10, "y": 1.28},
+    {"label": "I", "row": 1, "col": 8, "x": 11, "y": 1},
+    {"label": "O", "row": 1, "col": 9, "x": 12, "y": 1.31},
+    {"label": "P", "row": 1, "col": 10, "x": 13, "y": 1.93},
+    {"label": "\\", "row": 1, "col": 11, "x": 14, "y": 1.93},
+    {"label": "Meta", "row": 2, "col": 0, "x": 0, "y": 2.93},
+    {"label": "A", "row": 2, "col": 1, "x": 1, "y": 2.93},
+    {"label": "S", "row": 2, "col": 2, "x": 2, "y": 2.31},
+    {"label": "D", "row": 2, "col": 3, "x": 3, "y": 2},
+    {"label": "F", "row": 2, "col": 4, "x": 4, "y": 2.28},
+    {"label": "G", "row": 2, "col": 5, "x": 5, "y": 2.42},
+    {"label": "H", "row": 2, "col": 6, "x": 9, "y": 2.42},
+    {"label": "J", "row": 2, "col": 7, "x": 10, "y": 2.28},
+    {"label": "K", "row": 2, "col": 8, "x": 11, "y": 2},
+    {"label": "L", "row": 2, "col": 9, "x": 12, "y": 2.31},
+    {"label": ";", "row": 2, "col": 10, "x": 13, "y": 2.93},
+    {"label": "'", "row": 2, "col": 11, "x": 14, "y": 2.93},
+    {"label": "LShift", "row": 3, "col": 0, "x": 0, "y": 3.93},
+    {"label": "Z", "row": 3, "col": 1, "x": 1, "y": 3.93},
+    {"label": "X", "row": 3, "col": 2, "x": 2, "y": 3.31},
+    {"label": "C", "row": 3, "col": 3, "x": 3, "y": 3},
+    {"label": "V", "row": 3, "col": 4, "x": 4, "y": 3.28},
+    {"label": "B", "row": 3, "col": 5, "x": 5, "y": 3.42},
+    {"label": "N", "row": 3, "col": 6, "x": 9, "y": 3.42},
+    {"label": "M", "row": 3, "col": 7, "x": 10, "y": 3.28},
+    {"label": ",", "row": 3, "col": 8, "x": 11, "y": 3},
+    {"label": ".", "row": 3, "col": 9, "x": 12, "y": 3.31},
+    {"label": "/", "row": 3, "col": 10, "x": 13, "y": 3.93},
+    {"label": "RShift", "row": 3, "col": 11, "x": 14, "y": 3.93},
+    {"label": "Ctrl", "row": 4, "col": 3, "x": 4, "y": 4.31},
+    {"label": "LSpace", "row": 4, "col": 4, "x": 5, "y": 4.42},
+    {"label": "Raise", "row": 4, "col": 5, "x": 6, "y": 4.42},
+    {"label": "Lower", "row": 4, "col": 6, "x": 8, "y": 4.42},
+    {"label": "RSpace", "row": 4, "col": 7, "x": 9, "y": 4.42},
+    {"label": "Alt", "row": 4, "col": 8, "x": 10, "y": 4.31},
+]
+
+
+combos = """
+ws AT
+sd ENTER
+jk LPAR
+jm RPAR
+kl LBKT
+k, RBKT
+jl LBRC
+m. RBRC
+mk COLON
+ji EQUAL
+uj STAR
+ui UNDERSCORE
+cv LC(TAB)
+xc LC(LS(TAB))
+gf PLUS
+yh CARET
+we EXCL
+re AMPS
+rw PIPE
+ed HASH
+rf GRAVE 
+fd ESC
+sf BSLH
+tg DOLLAR
+vd MINUS
+fe TILDE
+m, PERC
+,. SEMICOL
+"""
+
+
+def tools(user_spec):
+    def decorator(func):
+        def wrap(*args, **kwargs):
+            kwargs["user_spec"] = "fdsf" if user_spec else "nono"
+            return func(*args, **kwargs)
+
+        return wrap
+
+    return decorator
+
+
+def fn(user_spec):
+    print(user_spec)
+
+
+@tools(False)
+def fn2(user_spec):
+    print(user_spec)
+
+
+if __name__ == "__main__":
+    ctr = 0
+    inf = []
+    map = dict()
+
+    out = ""
+    for key in keys:
+        inf.append([key["label"], ctr])
+        map[key["label"].lower()] = ctr
+        ctr += 1 if key["label"] != "B" else 3
+    for combo in combos.splitlines():
+        combo = combo.strip()
+        if combo:
+            keycode = combo.split()[1]
+            a = combo[0]
+            b = combo[1]
+            out += f"KOMBO({keycode.lower()}, &kp {keycode}, {map[a]} {map[b]})\n\n"
+
+    print(out)
